754839f41480a12a8d64b4576c479a801bae8d45
Jerry Bruwes <jbruwes@icloud.com>
2025-10-05 17:39:00 +0200

    Refactor SFC loader to optimize async processing and error handling

--- src/loader-sfc.ts
+++ src/loader-sfc.ts
@@ -1,4 +1,4 @@
-import type { Options } from "sucrase";
+import type { Options, Transform } from "sucrase";
 import type { Component } from "vue";
 import type { CompilerOptions } from "vue/compiler-sfc";
 
@@ -16,7 +16,6 @@ import {
 const fetchText = async (url: string) => {
     try {
       const response = await fetch(url);
-
       return response.ok ? response : new Response("");
     } catch {
       return new Response("");
@@ -24,111 +23,100 @@ const fetchText = async (url: string) => {
   },
   inject = async (code: string) => {
     const objectURL = URL.createObjectURL(
-        new Blob([code], { type: "application/javascript" }),
-      ),
-      value = (await import(objectURL)) as Record<string, object>;
-
-    URL.revokeObjectURL(objectURL);
-
-    return value;
-  },
-  log = (msgs: (Error | string)[]) => {
-    msgs.forEach((msg) => {
-      console.log(msg);
-    });
-  },
-  options: Options = {
-    jsxRuntime: "preserve",
-    transforms: ["jsx", "typescript"],
+      new Blob([code], { type: "application/javascript" }),
+    );
+    try {
+      return (await import(objectURL)) as Record<string, object>;
+    } finally {
+      URL.revokeObjectURL(objectURL);
+    }
   };
 
 /* -------------------------------------------------------------------------- */
 
 export default async (filename: string) => {
   const id = `data-v-${hash(filename)}`,
-    { descriptor, errors } = parse(
+    langs = new Set(),
+    { descriptor, errors: alerts } = parse(
       (await (await fetchText(filename)).text()) || "<template></template>",
     ),
     { script, scriptSetup, slotted, styles, template } = descriptor;
 
-  log(errors);
+  [script, scriptSetup].forEach((scriptBlock) => {
+    const { lang = "js" } = scriptBlock ?? {};
+    if (/[jt]sx$/.test(lang)) langs.add("jsx");
+    if (/tsx?$/.test(lang)) langs.add("typescript");
+  });
 
   const compilerOptions: CompilerOptions = {
-      expressionPlugins: ["jsx", "typescript"],
+      expressionPlugins: [...langs] as ("jsx" | "typescript")[],
       scopeId: id,
       slotted,
     },
     scoped = styles.some(({ scoped }) => scoped),
-    component: Component = scoped ? { __scopeId: id } : {};
-
-  if (!(document.getElementById(id) instanceof HTMLStyleElement)) {
-    const warnings = new Set<string>();
-
-    const textContent = (
-      await Promise.all(
-        styles.map(async ({ content, module, scoped = false, src }) => {
-          const { code, errors } = await compileStyleAsync({
-            filename,
-            id,
-            scoped,
-            source: src ? await (await fetchText(src)).text() : content,
-          });
-
-          if (module)
-            warnings.add("<style module> is not supported in the playground.");
-
-          log(errors);
-
-          return code;
-        }),
-      )
-    )
-      .join("\n")
-      .trim();
-
-    if (textContent) {
-      const el = document.createElement("style");
-      el.id = id;
-      el.textContent = textContent;
-      document.head.appendChild(el);
-    }
-
-    log([...warnings]);
+    component: Component = scoped ? { __scopeId: id } : {},
+    options: Options = {
+      jsxRuntime: "preserve",
+      transforms: [...langs] as Transform[],
+    },
+    style = !(document.getElementById(id) instanceof HTMLStyleElement)
+      ? Promise.all(
+          styles.map(
+            async ({ content, module = false, scoped = false, src }) => {
+              const { code, errors } = await compileStyleAsync({
+                filename,
+                id,
+                scoped,
+                source: src ? await (await fetchText(src)).text() : content,
+              });
+              if (module)
+                errors.push(
+                  Error("<style module> is not supported in the playground."),
+                );
+              alerts.push(...errors);
+              return code;
+            },
+          ),
+        )
+      : Promise.resolve([]),
+    { ast, content: source } = template ?? {},
+    { bindings, content, warnings } =
+      script || scriptSetup ? compileScript(descriptor, { id }) : {};
+  if (source && bindings) compilerOptions.bindingMetadata = bindings;
+  const { code, errors, tips } =
+    source !== undefined
+      ? compileTemplate({
+          ...(ast ?? {}),
+          compilerOptions,
+          filename,
+          id,
+          scoped,
+          slotted,
+          source,
+        })
+      : {};
+  const [styleResult, scriptResult, templateResult] = await Promise.all([
+      style,
+      content
+        ? inject(langs.size ? transform(content, options).code : content)
+        : Promise.resolve(undefined),
+      code
+        ? inject(langs.size ? transform(code, options).code : code)
+        : Promise.resolve(undefined),
+    ]),
+    textContent = styleResult.join("\n").trim();
+  if (textContent) {
+    const el = document.createElement("style");
+    el.id = id;
+    el.textContent = textContent;
+    document.head.appendChild(el);
   }
-
-  if (script || scriptSetup) {
-    const {
-      bindings,
-      content,
-      warnings = [],
-    } = compileScript(descriptor, { id });
-
-    if (template && bindings) compilerOptions.bindingMetadata = bindings;
-    Object.assign(
-      component,
-      (await inject(transform(content, options).code)).default,
-    );
-
-    log(warnings);
-  }
-
-  if (template) {
-    const { ast, content: source } = template,
-      { code, errors, tips } = compileTemplate({
-        ...(ast && { ast }),
-        compilerOptions,
-        filename,
-        id,
-        scoped,
-        slotted,
-        source,
-      });
-
-    Object.assign(component, await inject(transform(code, options).code));
-
-    log(tips);
-    log(errors);
-  }
-
+  Object.assign(component, scriptResult?.default);
+  Object.assign(component, templateResult);
+  [...alerts, ...(warnings ?? []), ...(tips ?? []), ...(errors ?? [])].forEach(
+    (msg) => {
+      console.log(msg);
+    },
+  );
   return component;
 };
