a5089d9017a22268501e7475ec4501bcb10dcca2
Jerry Bruwes <jbruwes@icloud.com>
2025-10-09 13:58:47 +0200

    Refactor Vue SFC loader with improved structure and error handling

--- src/loader-sfc.ts
+++ src/loader-sfc.ts
@@ -1,6 +1,5 @@
 import type { ParserPlugin } from "@babel/parser";
 import type { Options, Transform } from "sucrase";
-import type { Component } from "vue";
 import type {
   CompilerOptions,
   SFCAsyncStyleCompileOptions,
@@ -9,6 +8,7 @@ import type {
   SFCTemplateCompileOptions,
 } from "vue/compiler-sfc";
 
+import { consola } from "consola/browser";
 import hash from "hash-sum";
 import { transform } from "sucrase";
 import {
@@ -18,6 +18,8 @@ import {
   parse,
 } from "vue/compiler-sfc";
 
+/* -------------------------------------------------------------------------- */
+/*                              Служебные функции                             */
 /* -------------------------------------------------------------------------- */
 
 const fetchText = async (url: string) => {
@@ -40,6 +42,8 @@ const fetchText = async (url: string) => {
   };
 
 /* -------------------------------------------------------------------------- */
+/*                  Функция загрузки файла с компонентом Vue                  */
+/* -------------------------------------------------------------------------- */
 
 export default async (
   filename: string,
@@ -61,15 +65,27 @@ export default async (
         styleOptions?: Partial<SFCAsyncStyleCompileOptions>;
       } = {},
 ) => {
-  const styleErrors: Error[] = [],
-    { descriptor, errors: parseErrors } = parse(
-      (await (await fetchText(filename)).text()) || "<template></template>",
-      parseOptions,
-    ),
-    { script, scriptSetup, slotted, styles, template } = descriptor;
-  let moduleWarning = "";
-  const id = `data-v-${hash(filename)}`,
-    langs = new Set(
+  /* -------------------------------------------------------------------------- */
+  /*                    Генерация уникального идентификатора                    */
+  /* -------------------------------------------------------------------------- */
+
+  const id = `data-v-${hash(filename)}`;
+
+  /* -------------------------------------------------------------------------- */
+  /*                 Загрузка и парсинг файла с компонентом Vue                 */
+  /* -------------------------------------------------------------------------- */
+
+  const { descriptor, errors: parseErrors } = parse(
+    (await (await fetchText(filename)).text()) || "<template></template>",
+    { filename, ...parseOptions },
+  );
+
+  /* -------------------------------------------------------------------------- */
+  /*                         Обработка полученных данных                        */
+  /* -------------------------------------------------------------------------- */
+
+  const { script, scriptSetup, slotted, styles, template } = descriptor;
+  const langs = new Set(
       [script, scriptSetup].flatMap((scriptBlock) => {
         const { lang = "js" } = scriptBlock ?? {};
         return [
@@ -78,19 +94,55 @@ export default async (
         ] as ParserPlugin[];
       }),
     ),
-    compilerOptions: CompilerOptions = {
+    { ast, content: source = "" } = template ?? {};
+
+  /* -------------------------------------------------------------------------- */
+  /*                      Загрузка и компилирование стилей                      */
+  /* -------------------------------------------------------------------------- */
+
+  let styleWarning;
+  const styleErrors: Error[] = [];
+  const style = !(document.getElementById(id) instanceof HTMLStyleElement)
+    ? Promise.all(
+        styles.map(async ({ content, module, scoped = false, src }) => {
+          const modules = !!module;
+          if (modules) {
+            styleWarning = "<style module> is not supported in the playground.";
+            return "";
+          } else {
+            const { code, errors } = await compileStyleAsync({
+              filename,
+              id,
+              modules,
+              scoped,
+              source: src ? await (await fetchText(src)).text() : content,
+              ...styleOptions,
+            });
+            styleErrors.push(...errors);
+            return code;
+          }
+        }),
+      )
+    : Promise.resolve([]);
+
+  /* -------------------------------------------------------------------------- */
+  /*                             Подготовка настроек                            */
+  /* -------------------------------------------------------------------------- */
+
+  const compilerOptions: CompilerOptions = {
       expressionPlugins: [
         ...new Set([...(expressionPlugins ?? []), ...langs]),
       ] as ParserPlugin[],
+      filename,
       scopeId: id,
       slotted,
       ...restCompilerOptions,
     },
-    scoped = styles.some(({ scoped }) => scoped),
-    component: Component = scoped ? { __scopeId: id } : {},
     templateOptions: Partial<SFCTemplateCompileOptions> = {
       compilerOptions,
-      scoped,
+      filename,
+      id,
+      scoped: styles.some(({ scoped }) => scoped),
       slotted,
       ...restTemplateOptions,
     },
@@ -99,40 +151,31 @@ export default async (
       templateOptions,
       ...restScriptOptions,
     },
-    style = !(document.getElementById(id) instanceof HTMLStyleElement)
-      ? Promise.all(
-          styles.map(async ({ content, module, scoped = false, src }) => {
-            const modules = !!module;
-            if (modules) {
-              moduleWarning =
-                "<style module> is not supported in the playground.";
-              return "";
-            } else {
-              const { code, errors } = await compileStyleAsync({
-                filename,
-                id,
-                modules,
-                scoped,
-                source: src ? await (await fetchText(src)).text() : content,
-                ...styleOptions,
-              });
-              styleErrors.push(...errors);
-              return code;
-            }
-          }),
-        )
-      : Promise.resolve([]),
     sucraseOptions: Options = {
       jsxRuntime: "preserve",
       transforms: [...langs] as Transform[],
-    },
-    { ast, content: source = "" } = template ?? {},
-    {
-      bindings,
-      content,
-      warnings: scriptWarnings,
-    } = script || scriptSetup ? compileScript(descriptor, scriptOptions) : {};
+    };
+
+  /* -------------------------------------------------------------------------- */
+  /*                             Компиляция скрипта                             */
+  /* -------------------------------------------------------------------------- */
+
+  const {
+    bindings,
+    content,
+    warnings: scriptWarnings,
+  } = script || scriptSetup ? compileScript(descriptor, scriptOptions) : {};
+
+  /* -------------------------------------------------------------------------- */
+  /*                            Связывание метаданных                           */
+  /* -------------------------------------------------------------------------- */
+
   if (bindings) compilerOptions.bindingMetadata = bindings;
+
+  /* -------------------------------------------------------------------------- */
+  /*                             Компиляция шаблона                             */
+  /* -------------------------------------------------------------------------- */
+
   const {
     code,
     errors: templateErrors,
@@ -146,33 +189,44 @@ export default async (
         ...templateOptions,
       })
     : {};
+
+  /* -------------------------------------------------------------------------- */
+  /*                  Вывод ошибок, предупреждений и подсказок                  */
+  /* -------------------------------------------------------------------------- */
+
+  consola.error(parseErrors, templateErrors, styleErrors);
+  consola.warn(scriptWarnings, styleWarning);
+  consola.info(templateTips);
+
+  /* -------------------------------------------------------------------------- */
+  /*                      Получение и обработка результатов                     */
+  /* -------------------------------------------------------------------------- */
+
   const [styleResult, scriptResult, templateResult] = await Promise.all([
-      style,
-      content
-        ? inject(langs.size ? transform(content, sucraseOptions).code : content)
-        : Promise.resolve(undefined),
-      code
-        ? inject(langs.size ? transform(code, sucraseOptions).code : code)
-        : Promise.resolve(undefined),
-    ]),
-    textContent = styleResult.join("\n").trim();
+    style,
+    content
+      ? inject(langs.size ? transform(content, sucraseOptions).code : content)
+      : Promise.resolve(undefined),
+    code
+      ? inject(langs.size ? transform(code, sucraseOptions).code : code)
+      : Promise.resolve(undefined),
+  ]);
+
+  /* -------------------------------------------------------------------------- */
+  /*                         Внедрение стилей в документ                        */
+  /* -------------------------------------------------------------------------- */
+
+  const textContent = styleResult.join("\n").trim();
   if (textContent) {
     const el = document.createElement("style");
     el.id = id;
     el.textContent = textContent;
     document.head.appendChild(el);
   }
-  Object.assign(component, scriptResult?.default);
-  Object.assign(component, templateResult);
-  [
-    ...parseErrors,
-    ...(scriptWarnings ?? []),
-    ...(templateTips ?? []),
-    ...(templateErrors ?? []),
-    ...styleErrors,
-    ...(moduleWarning ? [moduleWarning] : []),
-  ].forEach((msg) => {
-    console.log(msg);
-  });
-  return component;
+
+  /* -------------------------------------------------------------------------- */
+  /*                    Формирование возвращаемого компонента                   */
+  /* -------------------------------------------------------------------------- */
+
+  return { __scopeId: id, ...scriptResult?.default, ...templateResult };
 };
