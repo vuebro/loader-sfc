8ef8a7c92515e18a9d11a1c8cf6e5311726eabbf
Jerry Bruwes <jbruwes@icloud.com>
2025-10-16 12:13:28 +0200

    Simplify Vite config and add Russian documentation comment

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
 import terser from "@rollup/plugin-terser";
-import { resolve } from "node:path";
 import { defineConfig } from "vite";
+
+/* -------------------------------------------------------------------------- */
+/*     Настройка vite для сборки библиотеки loader-sfc.esm-browser.prod.js    */
+/* -------------------------------------------------------------------------- */
 
 export default defineConfig({
   build: {
     emptyOutDir: false,
     lib: {
-      entry: resolve(__dirname, "src/loader-sfc.ts"),
+      entry: "src/loader-sfc.ts",
       fileName: "loader-sfc.esm-browser.prod",
       formats: ["es"],
     },
     rollupOptions: {
       external: ["vue"],
-      plugins: [terser({ format: { comments: false }, mangle: true })],
+      plugins: [terser()],
     },
   },
 });
